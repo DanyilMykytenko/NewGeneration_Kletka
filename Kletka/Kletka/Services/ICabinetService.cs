d78b956ab6aa240eb33a85a606049682c192463b
DaniylMykytenko <freezeee682@gmail.com>
2022-04-26 16:28:11 +0300

    Basic structure for personal cabinet service

---
+++ Kletka/Kletka/Services/ICabinetService.cs
@@ -0,0 +1,13 @@
+﻿using Kletka.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kletka.Services
+{
+    interface IPersonalCabinet
+    {
+        public Task uploadInformation(Accounts account);
+    }
+}
